d771870090a2902f2d12a328d90a15834ec5a5da
Glariance <faadi3media@gmail.com>
2025-11-22 09:59:52 +0500

    config cors changes.

--- config/cors.php
+++ config/cors.php
@@ -1,24 +1,16 @@
 <?php
 
 return [
-    'paths' => [
-        'api/*',
-        'sanctum/csrf-cookie',
-    ],
+'paths' => ['api/*', 'sanctum/csrf-cookie'],
+'allowed_methods' => ['*'],
+'allowed_origins' => [
+    'https://pureserenityshop.com',
+    'http://localhost:5173',
+],
+'allowed_origins_patterns' => [],
+'allowed_headers' => ['*'],
+'exposed_headers' => [],
+'max_age' => 0,
+'supports_credentials' => false,
 
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => array_filter(
-        explode(',', env('FRONTEND_ORIGINS', 'http://localhost:5173'))
-    ),
-
-    'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
-    'exposed_headers' => [],
-
-    'max_age' => 0,
-
-    'supports_credentials' => false,
 ];
